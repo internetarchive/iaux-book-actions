59d501fae38ba6da9f2503f0546b801cc53cea5a
Neeraj Sharma <neeraj@archive.org>
2021-11-30 13:17:55 +0530

    embed view tests fixes

--- test/components/book-title-bar.test.js
+++ test/components/book-title-bar.test.js
@@ -1,4 +1,4 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, oneEvent } from '@open-wc/testing';
 import '../../src/components/book-title-bar.js';
 
 const container = ({ identifier, bookTitle } = {}) =>
@@ -18,5 +18,22 @@ describe('<book-title-bar>', () => {
     const link = el.shadowRoot.querySelector('a');
     expect(link.innerText).to.equal('this is test booktitle');
     expect(link.getAttribute('href')).to.equal('/details/goody');
+  });
+
+  it('check if event if firing on valid analytics category action', async () => {
+    const el = await fixture(
+      container({
+        identifier: 'goody',
+        bookTitle: 'this is test booktitle',
+      })
+    );
+
+    const clickButton = () =>
+      el.shadowRoot.querySelector('a').dispatchEvent(new Event('click'));
+    setTimeout(clickButton);
+
+    const { detail } = await oneEvent(el, 'bookTitleBar');
+    expect(detail.event.category).to.equal('BookReader-Header');
+    expect(detail.event.action).to.equal('Book-Title-Bar');
   });
 });
