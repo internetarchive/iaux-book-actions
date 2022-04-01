4e28881beed9f74854ce50e768316d1346143d73
Neeraj Sharma <neeraj@archive.org>
2022-04-01 18:01:07 +0530

    Failed to fetch error on sentry

--- src/core/services/actions-handler/actions-handler-service.js
+++ src/core/services/actions-handler/actions-handler-service.js
@@ -4,7 +4,7 @@
  * Helper to call loan service
  * @param {Object} options
  */
-export default function ActionsHandlerService(options) {
+export default async function ActionsHandlerService(options) {
   const option = {
     action: null,
     identifier: '',
@@ -15,54 +15,53 @@ export default function ActionsHandlerService(options) {
 
   let baseHost = '/services/loans/loan';
   const location = window?.location;
+  if (location?.pathname === '/demo1/') baseHost = `/demo/`;
 
-  // return intentional error on dev environment
+  // return error reponse when not production and has ?error=true param...
+  const tokenError = 'loan token not found. please try again later.';
+  const borrowError =
+    'This book is not available to borrow at this time. Please try again later.';
+  const erroneousActions = ['browse_book', 'borrow_book', 'create_token'];
   const shouldReturnError =
     location?.href?.indexOf('?error=true') !== -1 &&
     location?.hostname !== 'archive.org';
 
-  if (location?.pathname === '/demo/') baseHost = `/demo/`;
-
   let formData = new FormData();
   formData.append('action', option.action);
   formData.append('identifier', option.identifier);
-
-  fetch(baseHost, {
-    method: 'POST',
-    body: formData,
-  })
-    .then(response => {
-      const tokenError = 'loan token not found. please try again later.';
-      const borrowError =
-        'This book is not available to borrow at this time. Please try again later.';
-
-      // return error reponse when not production and has ?error=true param...
-      const erroneousActions = ['browse_book', 'borrow_book', 'create_token'];
-      if (shouldReturnError && erroneousActions.includes(option?.action)) {
-        return {
-          success: false,
-          error: option?.action === 'create_token' ? tokenError : borrowError,
-        };
-      }
-
-      // return success response for /demo/ server...
-      if (baseHost == '/demo/1' || baseHost == '/demo/') {
-        return {
-          success: true,
-          message: 'operation executed successfully!',
-        };
-      }
-
-      // The response is a Response instance.
-      // You parse the data into a useable format using `.json()`
-      return response.json();
+  try {
+    await fetch(baseHost, {
+      method: 'POST',
+      body: formData,
     })
-    .then(data => {
-      // `data` is the parsed version of the JSON returned from the above endpoint.
-      if (!data?.error) {
-        option?.success(data);
-      } else {
-        option?.error(data);
-      }
-    });
+      .then(response => {
+        // intentional error on localhost
+        if (shouldReturnError && erroneousActions.includes(option?.action)) {
+          return {
+            success: false,
+            error: option?.action === 'create_token' ? tokenError : borrowError,
+          };
+        }
+
+        // return success response for /demo/ server...
+        if (baseHost == '/demo/1' || baseHost == '/demo/') {
+          return {
+            success: true,
+            message: 'operation executed successfully!',
+          };
+        }
+
+        // The response is a Response instance.
+        // You parse the data into a useable format using `.json()`
+        return response.json();
+      })
+      .then(data => {
+        // `data` is the parsed version of the JSON returned from the above endpoint.
+        if (!data?.error) {
+          option?.success(data);
+        } else {
+          option?.error(data);
+        }
+      });
+  } catch (err) {}
 }
