ef019fe3140195a31c1ad832c648468ac73f58cc
Neeraj Sharma <neeraj@archive.org>
2023-11-29 01:12:21 +0530

    Lending bar - Admin Access button should keep ?q= param

--- src/core/services/actions-config.js
+++ src/core/services/actions-config.js
@@ -202,11 +202,15 @@ export default class ActionsConfig {
     if (this.lendingStatus.user_has_borrowed || !this.lendingStatus.isAdmin)
       return null;
 
+    // keep existing params and add new one
+    const url = new URL(window.location.href);
+    url.searchParams.append('admin', 1);
+
     return {
       id: 'adminAccess',
       text: 'Admin Access',
       title: 'You have administrative privileges to read this book',
-      url: '?admin=1',
+      url: url.search,
       target: '_self',
       className: 'danger',
       analyticsEvent: {
